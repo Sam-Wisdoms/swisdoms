63565efd5b55ae0c700d453be79d2af461280f80
Sam-Wisdoms Amenyenu <sampson.aglanu@gmail.com>
2024-01-28 22:21:41 +0000

    Added Header Component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+function Header(){
+    return (
+       <nav>
+        <h1 className="name">Sam-Wisdoms Amenyenu</h1>
+        <ul className="nav-items">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about"></Link></li>
+            <li><Link to="/blog"></Link></li>
+            <li><Link to="/projects"></Link></li>
+            <li><Link to="/contact"></Link></li>
+        </ul>
+       </nav>
+    )
+}
+
+export default Header
