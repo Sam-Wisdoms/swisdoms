c7eb094ef7241a932391587d67e89a4acb1fb748
Sam-Wisdoms Amenyenu <sampson.aglanu@gmail.com>
2024-01-28 23:00:07 +0000

    Added Router and Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Header from "./components/header/Header"
+
+import { BrowserRouter as Rounter, Route, Router, Routes } from "react-router-dom"
+import Header from "./components/header/Header.jsx"
+import Home from "./components/home/Home.jsx"
 
 function App() {
   return (
-    <>
-   <Header />
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/blog" element={ <Blog /> } />
+        <Route path="/projects" element={ <ProjectGallery /> } />
+        <Route path="/contact" element={ <Contact /> } />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+        <p>Highly motivated, hardworking and result-oriented Junior Front-End Developer with deep interest in Crypto Currencies, crypto trading, non-profit, global development and improved organizational systems, processes and procedures. I am looking forward to contribute my skills to a tech company with global reach while at the same time improving my skills.</p>
+        </div>
+    )
+}
+
+export default Home
